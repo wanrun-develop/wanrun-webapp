6d76c7dc3069a02b35bb78f925c6ce72c01b791b
sugiwa <sugi.warra@gmail.com>
2024-09-03 23:57:52 +0900

    リクエスト周りの統一

--- src/app/(default)/dog/hooks/useCreateDog.ts
+++ src/app/(default)/dog/hooks/useCreateDog.ts
@@ -1,4 +1,4 @@
-import { post } from '@/libs/fetch';
+import useApi from '@/libs/useApi';
 import { DogFormSchema } from '@/schemas/DogSchema';
 import { DogFormType } from '@/types/Dog';
 import { useState } from 'react';
@@ -6,6 +6,7 @@ import { useState } from 'react';
 const useCreateDog = () => {
   const [isLoading, setIsLoading] = useState<boolean>(false);
   const [error, setError] = useState<string | null>(null);
+  const { api } = useApi();
 
   const createDog = async (params: DogFormType) => {
     setIsLoading(true);
@@ -13,7 +14,7 @@ const useCreateDog = () => {
 
     try {
       const request = DogFormSchema.parse(params);
-      const res = await post('/dog/create', request);
+      const res = await api('POST', '/dog/create', request);
       return res;
     } catch (error: any) {
       setError(error.message);
