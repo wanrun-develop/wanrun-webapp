fd00d5a13509423440a86c615de1eb738c1f2db2
sugiwa <sugi.warra@gmail.com>
2024-09-13 15:47:40 +0900

    犬一覧表示用コンポーネント作成

---
+++ src/app/(default)/dog/mock/dogData.ts
@@ -0,0 +1,26 @@
+export const dogs = [
+  {
+    id: 1,
+    name: 'test1',
+    sex: 'M',
+    size: '4kg',
+  },
+  {
+    id: 2,
+    name: 'test2',
+    sex: 'F',
+    size: '2kg',
+  },
+  {
+    id: 3,
+    name: 'test3',
+    sex: 'M',
+    size: '3kg',
+  },
+  {
+    id: 4,
+    name: 'test4',
+    sex: 'F',
+    size: '4kg',
+  },
+];
