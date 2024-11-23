d3b69d59fa6ae85d196aa52fa947855130b4ee23
sugiwa <sugi.warra@gmail.com>
2024-11-23 19:53:54 +0900

    犬一覧画面修正

--- src/app/(default)/dog/components/DogList/index.tsx
+++ src/app/(default)/dog/components/DogList/index.tsx
@@ -15,20 +15,24 @@ const DogList = () => {
   return (
     <div className={styles.container}>
       <table className={styles.table}>
-        <tr>
-          <th>ID</th>
-          <th>名前</th>
-          <th>体重</th>
-          <th>性別</th>
-        </tr>
-        {dogs.map((dog, i) => (
-          <tr key={i} onClick={() => redirectDetailPage(dog.id)}>
-            <td>{dog.id}</td>
-            <td>{dog.name}</td>
-            <td>{dog.weight}kg</td>
-            <td>{dog.sex}</td>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>名前</th>
+            <th>体重</th>
+            <th>性別</th>
           </tr>
-        ))}
+        </thead>
+        <tbody>
+          {dogs.map((dog, i) => (
+            <tr key={i} onClick={() => redirectDetailPage(dog.id)}>
+              <td>{dog.id}</td>
+              <td>{dog.name}</td>
+              <td>{dog.weight}kg</td>
+              <td>{dog.sex}</td>
+            </tr>
+          ))}
+        </tbody>
       </table>
     </div>
   );
